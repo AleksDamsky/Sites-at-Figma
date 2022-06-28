d5bc8238d8f94aad1dc70dfc53800de45ea0ebac
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-06-28 18:23:18 +0300

    I finished this small site

---
+++ JavaScript/dinamica.js
@@ -0,0 +1,12 @@
+/*Объекты*/
+
+let object = {
+  name : ['Aleks', 'Hripnv'],
+  age : 21,
+  gender : 'man',
+  bio : () => {
+    console.log(`My name is: ${object.name[0]} and ${object.age} old`);
+  } 
+  };
+
+// console.log(object);
